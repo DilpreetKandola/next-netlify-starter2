a5d4fe1899c50b631514f8e3b1b3029839af9e76
DilpreetKandola <97153403+DilpreetKandola@users.noreply.github.com>
2022-01-14 05:58:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+</body>
+<script>
+name = prompt("What is your name?");
+favcolor = prompt("what is your favorite Color?");
+document.write("My name is " +name + " and my favorite color is " +favcolor)
+</script>
+</html>
